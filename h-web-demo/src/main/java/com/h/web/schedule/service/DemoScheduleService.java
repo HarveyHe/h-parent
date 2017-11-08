fc521a469d36f124a9ce670d4f0cc8d92cdb3f99
Harvey.He <Harvey.He@GSST-HARVEY-HE.gsst.com>
2017-11-08 10:44:33 +0800

    添加定时器demo代码

---
+++ h-web-demo/src/main/java/com/h/web/schedule/service/DemoScheduleService.java
@@ -0,0 +1,11 @@
+package com.h.web.schedule.service;
+
+public interface DemoScheduleService {
+
+	
+	/**
+	 * 定时测试
+	 * */
+
+	void test();
+}
---
+++ h-web-demo/src/main/java/com/h/web/schedule/service/impl/DemoScheduleServiceImpl.java
@@ -0,0 +1,21 @@
+package com.h.web.schedule.service.impl;
+
+import java.util.Date;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import com.h.web.schedule.service.DemoScheduleService;
+
+@Service
+public class DemoScheduleServiceImpl implements DemoScheduleService {
+
+
+	@Override
+	@Scheduled(cron="*/30 * * * * *")
+	public void test() {
+		Date now = new Date();
+		System.out.println(now);
+	}
+
+}
